cfd5683b58aaf656ba7d69a159df370ba62f4f2f
Jace Laquerre <jace.laquerre@uvm.edu>
2020-02-27 22:04:33 -0500

    Update Assignment.java

--- src/Assignment.java
+++ src/Assignment.java
@@ -6,9 +6,6 @@ public class Assignment {
     private static final int SMALL_SIDED = 12;
 
     public static void main(String[] args) {
-        FileIO file = new FileIO();
-        //file.refFileIO();
-        //file.fieldFileIO();
         Assignment assignment = new Assignment();
         assignment.assignment();
     }
