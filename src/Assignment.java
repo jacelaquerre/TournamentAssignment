4abc821fa3542005365540695972c91fad5b8a21
Jace Laquerre <jace.laquerre@uvm.edu>
2020-02-27 22:04:44 -0500

    Update Assignment.java

--- src/Assignment.java
+++ src/Assignment.java
@@ -3,8 +3,6 @@ import java.util.List;
 
 public class Assignment {
 
-    private static final int SMALL_SIDED = 12;
-
     public static void main(String[] args) {
         Assignment assignment = new Assignment();
         assignment.assignment();
